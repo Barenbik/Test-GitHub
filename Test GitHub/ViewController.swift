3d53e4afa49b85056049d4b9616e14ad7c52ccbf
Tony Sharples <tonysharples22@me.com>
2018-04-17 12:30:30 +0100

    Initial Commit

---
+++ Test GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test GitHub
+//
+//  Created by Tony Sharples on 17/04/2018.
+//  Copyright © 2018 Tony Sharples. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
