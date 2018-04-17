15ff6aaf5ea3b305548a3f10441d7a12eff9aba7
Tony Sharples <tonysharples22@me.com>
2018-04-17 12:37:05 +0100

    Added a test comment

--- Test GitHub/ViewController.swift
+++ Test GitHub/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Add a comment to test the branching.
     }
 
     override func didReceiveMemoryWarning() {
